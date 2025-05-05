23926d982b44044216162b424f61306d5c6e8fa7
Oleg Ianchenko <olegian@cs.washington.edu>
2025-05-04 18:31:58 -0700

    comment hooking still broken

--- src/app/[doc]/Threads.tsx
+++ src/app/[doc]/Threads.tsx
@@ -1,20 +1,90 @@
 import {
-    FloatingComposer,
-    FloatingThreads,
-    FloatingToolbar,
-    Toolbar,
+  FloatingComposer,
+  FloatingThreads,
+  FloatingToolbar,
+  Toolbar,
 } from "@liveblocks/react-tiptap";
-import { Comment } from "@liveblocks/react-ui/primitives";
+import {
+  Comment,
+  ComposerSubmitComment,
+} from "@liveblocks/react-ui/primitives";
 import { useThreads } from "@liveblocks/react/suspense";
 import { type Editor } from "@tiptap/react";
+import { deleteAnnotation } from "../actions";
+import { useEffect, useState } from "react";
+
+interface Annotation {
+  from: number;
+  to: number;
+  comment: string | undefined;
+}
 
 export function Threads({ editor }: { editor: Editor | null }) {
   const { threads } = useThreads({ query: { resolved: false } });
+  const [annotations, setAnnotations] = useState<Annotation[]>([]);
 
-  const annotationHandler = () => {
+  const newAnnotationHandler = () => {
     // pending comment tells composer to open up
-    editor?.chain().focus().addPendingComment().run();
+    if (!editor) {
+      return;
+    }
+    editor.chain().focus().addPendingComment().run();
   };
+
+  const getSelection = () => {
+    if (!editor) return { text: "", from: -1, to: -1 };
+
+    const { view, state } = editor;
+    const { from, to } = view.state.selection;
+    const text = state.doc.textBetween(from, to, "");
+    return { text, from, to };
+  };
+
+  const composerSubmitHandler = (
+    comment: ComposerSubmitComment,
+    event: any
+  ) => {
+    const { from, to } = getSelection();
+    const annotation = comment.body.content[0].children[0].text;
+
+    setAnnotations((old) => [
+      ...old,
+      {
+        from: from,
+        to: to,
+        comment: annotation,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(annotations);
+  }, [annotations]);
+
+  // populate annotations state on mount
+  useEffect(() => {
+    let annotations: Annotation[] = threads.map((thread) => {
+      const text = thread.comments[0].body?.content[0].children[0].text;
+
+      return {
+        comment: text,
+        from: thread.metadata["from"],
+        to: thread.metadata["to"],
+      };
+    });
+
+    setAnnotations((old) => {
+      return annotations;
+    });
+
+    let listener = editor?.on("update", () => {
+        console.log("changed")
+    })
+
+    return () => {
+        listener?.destroy()
+    }
+  }, []);
 
   return (
     <>
@@ -23,7 +93,7 @@ export function Threads({ editor }: { editor: Editor | null }) {
         <Toolbar.Button
           name={"Annotate"}
           shortcut="CMD-H"
-          onClick={annotationHandler}
+          onClick={newAnnotationHandler}
         />
       </FloatingToolbar>
 
@@ -35,11 +105,21 @@ export function Threads({ editor }: { editor: Editor | null }) {
             // TODO: add another button to this div to delete the annotation
             <div className={"annotation-container"}>
               {props.thread.comments.map((comment) => (
-                <Comment.Body
-                  key={comment.id}
-                  body={comment.body}
-                  className={"annotation"}
-                />
+                <div key={comment.id}>
+                  <Comment.Body body={comment.body} className={"annotation"} />
+                  <button
+                    onClick={() => {
+                      const response = deleteAnnotation(
+                        props.thread.roomId,
+                        props.thread.id,
+                        comment.id
+                      );
+                      console.log(response);
+                    }}
+                  >
+                    Delete Annotation
+                  </button>
+                </div>
               ))}
             </div>
           ),
@@ -47,12 +127,21 @@ export function Threads({ editor }: { editor: Editor | null }) {
       />
 
       <FloatingComposer
-        overrides={{ COMPOSER_PLACEHOLDER: "Write an annotation..." }}
+        overrides={{
+          COMPOSER_PLACEHOLDER: "Write an annotation...",
+        }}
         editor={editor}
         className="floating-composer"
         style={{ width: "350px" }}
         showAttachments={false}
         showFormattingControls={false}
+        onComposerSubmit={composerSubmitHandler}
+        metadata={{
+          // wow this is atrocious with the triple call
+          hook: getSelection()["text"],
+          from: getSelection()["from"],
+          to: getSelection()["to"],
+        }}
       />
     </>
   );
--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,4 +1,7 @@
-"use server"
+"use server";
+
+const LB_DELETE_COMMENT_URL =
+  "https://api.liveblocks.io/v2/rooms/{room_id}/threads/{thread_id}/comments/{comment_id}";
 
 // TODO: probably change the data to be of whatever type captures required context,
 // then actually invoke the LLM and return something back to the client.
@@ -6,6 +9,25 @@
 // this probably requires a private API key, which is cool cause the use server protects us anyways.
 // https://liveblocks.io/docs/api-reference/rest-api-endpoints >> Storage section
 export async function prompt(doc_name: string, data: string) {
-    console.log(`<PROMPT> ${doc_name} => ${data}`)
-    return "from server!"
+  console.log(`<PROMPT> ${doc_name} => ${data}`);
+  return "from server!";
+}
+
+export async function deleteAnnotation(
+  room_id: string,
+  threadId: string,
+  commentId: string
+) {
+  const endpoint = LB_DELETE_COMMENT_URL.replace("{room_id}", room_id)
+    .replace("{thread_id}", threadId)
+    .replace("{comment_id}", commentId);
+
+  const response = await fetch(endpoint, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${process.env.LB_KEY}`,
+    },
+  });
+
+  return response.status;
 }
