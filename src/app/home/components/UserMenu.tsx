3671497806d38598ef323749e6ce0b0d38ced118
Ritesh Kanchi <ritesh.kanchi@gmail.com>
2025-05-10 02:33:18 -0700

    user-specific colors

---
+++ src/app/home/components/UserMenu.tsx
@@ -0,0 +1,60 @@
+import { signOut, users } from "@/app/auth";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
+import { UserCircleIcon } from "@heroicons/react/20/solid";
+import { User } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default function UserMenu({ user }: { user: User | null }) {
+  const profile = users.find((u) => u.username === user?.id);
+  const [first = "", last = ""] = profile?.name.split(" ") || [];
+  return (
+    <Menu>
+      <MenuButton>
+        {user && user.name ? (
+          <div
+            style={{
+              backgroundColor: profile?.color,
+            }}
+            className="uppercase flex items-center justify-center size-10 rounded-full text-white font-semibold text-base"
+          >
+            {first.charAt(0)}
+            {last.charAt(0)}
+          </div>
+        ) : (
+          <UserCircleIcon className="size-10 fill-zinc-500 animate-pulse" />
+        )}
+      </MenuButton>
+      <MenuItems
+        transition
+        anchor="bottom end"
+        className="w-52 z-50 origin-top-right rounded-xl border border-zinc-200 bg-white p-1 text-sm/6 text-zinc-700 shadow-xl transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
+      >
+        <MenuItem>
+          <button
+            onClick={() => {
+              signOut();
+              redirect("/");
+            }}
+            className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-zinc-100 font-medium"
+          >
+            {profile?.name}
+          </button>
+        </MenuItem>
+        <div className="my-1 h-px bg-zinc-200" />
+        <MenuItem>
+          <button
+            onClick={() => {
+              signOut();
+              redirect("/");
+            }}
+            className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-zinc-100 font-medium"
+          >
+            {/* <TrashIcon className="size-4 fill-red-500" /> */}
+            Sign out
+          </button>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
+  );
+}
