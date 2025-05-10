b794039443a331a9d29f3113bedcdaab00bd216a
Ritesh Kanchi <ritesh.kanchi@gmail.com>
2025-05-09 23:21:56 -0700

    non piub key

--- src/app/liveblocks.ts
+++ src/app/liveblocks.ts
@@ -1,5 +1,5 @@
 import { Liveblocks } from "@liveblocks/node";
 
 export const liveblocks = new Liveblocks({
-  secret: process.env.NEXT_PUBLIC_LB_KEY ?? "",
+  secret: process.env.LB_KEY ?? "",
 });
